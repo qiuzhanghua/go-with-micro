af565bfc2234d691cc90f95efb74cedc2d2436f2
Daniel <qiuzhanghua@icloud.com>
2020-04-04 22:54:40 +0800

    toy run

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/qiuzhanghua/go-with-micro
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/golang/protobuf v1.3.5
 	github.com/micro/go-micro/v2 v2.4.0
+	github.com/micro/go-plugins v1.5.1 // indirect
+	github.com/micro/protoc-gen-micro/v2 v2.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
-	"github.com/micro/go-micro/v2"
+	"context"
+	"fmt"
+	//	_ "github.com/micro/go-plugins/registry/consul"
+
+	micro "github.com/micro/go-micro/v2"
+	proto "github.com/qiuzhanghua/go-with-micro/proto"
 )
 
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *proto.Request, rsp *proto.Response) error {
+	rsp.Greeting = "Hello " + req.Name
+	return nil
+}
+
 func main() {
+	//	registry := consul.NewRegistry()
+	// Create a new service. Optionally include some options here.
 	service := micro.NewService(
-		// Set service name
-		micro.Name("my.service"),
+		micro.Name("greeter"),
+		//		micro.Registry(registry),
 	)
 
-	// Parse CLI flags
+	// Init will parse the command line flags.
 	service.Init()
+
+	// Register handler
+	proto.RegisterGreeterHandler(service.Server(), new(Greeter))
+
+	// Run the server
+	if err := service.Run(); err != nil {
+		fmt.Println(err)
+	}
 }
